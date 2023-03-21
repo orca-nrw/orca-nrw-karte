df2b344506d7a4759a843b0db32290c8f838fbaf
mrkwnzl <40534855+mrkwnzl@users.noreply.github.com>
2023-03-21 11:06:40 +0100

    Added sidebar

--- scripts/lists/netzwerk-liste.js
+++ scripts/lists/netzwerk-liste.js
@@ -1,63 +1,69 @@
 import {standortObjekt} from "../maps/netzwerk-karte.js";
 
-window.standortObjekt = standortObjekt;
-
-let fetchData = await fetch("./db/standorte.json");
-let standorte = await fetchData.json();
-
-let content = "";
-
-standorte.sort(byNameAscending);
-
-function byNameAscending(itemA, itemB) {
-  let nameA = itemA.name.toLowerCase();
-  let nameB = itemB.name.toLowerCase();
-
-  if (nameA < nameB) {
-    return -1;
-  } else if (nameA > nameB) {
-    return 1;
-  } else {
-    return 0;
-  }
+if (document.getElementById("list")) {
+  document.getElementById("list").innerHTML = await createContent();
 }
 
-for (let standort of standorte) {
-  let summary = `<summary class='location-list'>${standort.name}</summary>`;
-  let website = `<a href='${standort.website}' target='_blank'>Website</a>`;
+export async function createContent() {
 
-  let listePersonen = "";
-  let ueberschriftPersonen = "";
-  if (standort.kontaktpersonen.length >= 1) {
-    ueberschriftPersonen = (standort.kontaktpersonen.length > 1) ? "<hr><b>Netzwerkstelle ORCA.nrw</b>" : "<hr><b>Netzwerkstelle ORCA.nrw</b>";
-    for (let person of standort.kontaktpersonen) {
-      let name = (person.website) ? `<br><a href='${person.website}' target='_blank'>${person.name}</a>` : `<br>${person.name}`;
-      let mail = (person.mail) ? ` <a href='mailto:${person.mail}'><i class="far fa-envelope"></i></a>` : "";
-      listePersonen += name + mail;
+  window.standortObjekt = standortObjekt;
+
+  let fetchData = await fetch("./db/standorte.json");
+  let standorte = await fetchData.json();
+
+  standorte.sort(byNameAscending);
+
+  function byNameAscending(itemA, itemB) {
+    let nameA = itemA.name.toLowerCase();
+    let nameB = itemB.name.toLowerCase();
+
+    if (nameA < nameB) {
+      return -1;
+    } else if (nameA > nameB) {
+      return 1;
+    } else {
+      return 0;
     }
   }
-  let blockPersonen = ueberschriftPersonen + listePersonen;
 
-  let policyVeroeffentlichung = "";
-  if (standort.phase == 0) {
-    policyVeroeffentlichung = "Noch keine Policy";
-  } else if (standort.phase == 1) {
-    policyVeroeffentlichung = "In Entwicklungsphase";
-  } else if (standort.phase == 2) {
-    policyVeroeffentlichung = "In Gremienphase";
-  } else if (standort.phase == 3) {
-    policyVeroeffentlichung = `Veröffentlicht am ${standort.policyVeroeffentlichung}`;
+  let data = "";
+  for (let standort of standorte) {
+    let summary = `<summary class='location-list'>${standort.name}</summary>`;
+    let website = `<a href='${standort.website}' target='_blank'>Website</a>`;
+
+    let listePersonen = "";
+    let ueberschriftPersonen = "";
+    if (standort.kontaktpersonen.length >= 1) {
+      ueberschriftPersonen = (standort.kontaktpersonen.length > 1) ? "<hr><b>Netzwerkstelle ORCA.nrw</b>" : "<hr><b>Netzwerkstelle ORCA.nrw</b>";
+      for (let person of standort.kontaktpersonen) {
+        let name = (person.website) ? `<br><a href='${person.website}' target='_blank'>${person.name}</a>` : `<br>${person.name}`;
+        let mail = (person.mail) ? ` <a href='mailto:${person.mail}'><i class="far fa-envelope"></i></a>` : "";
+        listePersonen += name + mail;
+      }
+    }
+    let blockPersonen = ueberschriftPersonen + listePersonen;
+
+    let policyVeroeffentlichung = "";
+    if (standort.phase == 0) {
+      policyVeroeffentlichung = "Noch keine Policy";
+    } else if (standort.phase == 1) {
+      policyVeroeffentlichung = "In Entwicklungsphase";
+    } else if (standort.phase == 2) {
+      policyVeroeffentlichung = "In Gremienphase";
+    } else if (standort.phase == 3) {
+      policyVeroeffentlichung = `Veröffentlicht am ${standort.policyVeroeffentlichung}`;
+    }
+
+    let linkPopup = "";
+    if (document.title != "ORCA-Netzwerk und OER Policies an Hochschulen in NRW – Auflistung") {
+      linkPopup = ` | <a id="hello" href="#" onclick="standortObjekt['${standort.id}'].openPopup()">Auf Karte zeigen</a>`;
+    }
+
+    const link = (standort.policyLink) ? `<br><a href='${standort.policyLink}' target='_blank'>Link</a>` : "";
+    const blockPolicy = "<hr><b>OER Policy</b><br>" + policyVeroeffentlichung + link;
+
+    data += "<details class='location-list'>" + summary + website + linkPopup + blockPersonen + blockPolicy + "</details>";
   }
 
-  let linkPopup = "";
-  if (document.title != "ORCA-Netzwerk und OER Policies an Hochschulen in NRW – Auflistung") {
-    linkPopup = ` | <a id="hello" href="#" onclick="standortObjekt['${standort.id}'].openPopup()">Auf Karte zeigen</a>`;
-  }
-
-  const link = (standort.policyLink) ? `<br><a href='${standort.policyLink}' target='_blank'>Link</a>` : "";
-  const blockPolicy = "<hr><b>OER Policy</b><br>" + policyVeroeffentlichung + link;
-
-  content += "<details class='location-list'>" + summary + website + linkPopup + blockPersonen + blockPolicy + "</details>";
+  return data;
 }
-
-document.getElementById("list").innerHTML = content;
