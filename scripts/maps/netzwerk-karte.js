ecd543bdfb905e3224f759be1b76bc1df4f7275e
ORCA.nrw <113124356+benedictbecker@users.noreply.github.com>
2025-01-07 15:56:29 +0100

    Update netzwerk-karte.js

--- scripts/maps/netzwerk-karte.js
+++ scripts/maps/netzwerk-karte.js
@@ -81,7 +81,7 @@ for (let standort of standorte) {
   let listePersonen = "";
   let ueberschriftPersonen = "";
   if (standort.kontaktpersonen.length >= 1) {
-    ueberschriftPersonen = (standort.kontaktpersonen.length > 1) ? "<hr>Netzwerkstelle ORCA.nrw<b></b>" : " ";
+    ueberschriftPersonen = (standort.kontaktpersonen.length > 1) ? "<hr>Netzwerkstelle ORCA.nrw<b></b>" : "<hr> <b></b>";
     for (let person of standort.kontaktpersonen) {
       let name = (person.website) ? `<br><a href='${person.website}' target='_blank'>${person.name}</a>` : `<br>${person.name}`;
       let mail = (person.mail) ? ` <a href='mailto:${person.mail}'><i class="far fa-envelope"></i></a>` : "";
